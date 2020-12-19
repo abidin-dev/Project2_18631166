903f74f8febc3bbbd87b013f5f29a9929c8cdb1a
abidin-dev <56331377+abidin-dev@users.noreply.github.com>
2020-12-19 22:14:21 +0800

    Menambahkan method di MainActivity, menambahkan AddAcitiviy dan EditActivity

---
+++ app/src/main/java/com/abidin/uangkas1/helper/CurrentDate.java
@@ -0,0 +1,11 @@
+package com.abidin.uangkas1.helper;
+
+import java.util.Calendar;
+
+public class CurrentDate {
+
+    public static Calendar calendar = Calendar.getInstance();
+    public static int year  = calendar.get(calendar.YEAR);
+    public static int month = calendar.get(calendar.MONTH);
+    public static int day   = calendar.get(calendar.DAY_OF_MONTH);
+}
